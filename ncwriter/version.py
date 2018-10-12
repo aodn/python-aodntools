ff405050886190094fda130cdc194fb453a26cbf
mhidas <marty.hidas@utas.edu.au>
2018-10-12 16:35:48 +1100

    WIP: create setup.py, version.py and update requirements

---
+++ ncwriter/version.py
@@ -0,0 +1,5 @@
+"""Contains the current version tag, stored in the module level variable '__version__'.
+Updated automatically by the build server
+"""
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from ncwriter.version import __version__
+
+INSTALL_REQUIRES = [line.strip() for line in open('requirements.txt')]
+# INSTALL_REQUIRES = [
+#     'jsonschema==2.6.0',
+#     'numpy>=1.13.0',
+#     'netCDF4'
+# ]
+
+# TODO: add this when we have JSON schema files and templates bundled
+# PACKAGE_DATA = {
+#     'ncwriter': [
+#     ]
+# }
+
+PACKAGE_EXCLUDES = ['test_ncwrwiter.*', 'test_ncwriter']
+PACKAGE_NAME = 'ncwriter'
+
+setup(
+    name=PACKAGE_NAME,
+    version=__version__,
+    packages=find_packages(exclude=PACKAGE_EXCLUDES),
+    # package_data=PACKAGE_DATA,
+    url='https://github.com/aodn',
+    license='GPLv3',
+    author='AODN',
+    author_email='projectofficers@emii.org.au',
+    description='AODN netCDF tools library',
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    test_suite='test_ncwriter'
+)

09163430b1f7d44c1e20076b1d9acb269d840540
Leigh Gordon <lwgordonimos@users.noreply.github.com>
2019-09-04 16:19:03 +1000

    Revert "Add ncwriter alias module for compatibility with old namespace"

--- ncwriter/__init__.py
+++
@@ -1,3 +0,0 @@
-# Compatibility package, to allow 'import ncwriter' and 'from ncwriter import <object>' in existing code to work after
-# the package was moved under the 'aodntools' namespace
-from aodntools.ncwriter import *

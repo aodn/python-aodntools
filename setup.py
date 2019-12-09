c2eca42a8b7a9700de6279b1b9d838330c0a37c3
mhidas <marty.hidas@utas.edu.au>
2019-12-09 16:20:44 +1100

    add python version required and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,18 @@ setup(
     author_email='projectofficers@emii.org.au',
     description='AODN data tools library',
     zip_safe=False,
+    python_requires='>=3.5',
     install_requires=INSTALL_REQUIRES,
-    test_suite='test_aodntools'
+    test_suite='test_aodntools',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
 )

3236531b1a53996e961e027ae9a1952fd0af31e4
aodn-ci <info@aodn.org.au>
2020-01-30 00:08:11 +0000

    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ PACKAGE_NAME = 'aodntools'
 
 setup(
     name=PACKAGE_NAME,
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(exclude=PACKAGE_EXCLUDES),
     package_data=PACKAGE_DATA,
     url='https://github.com/aodn',

334262ca9e97cecebcec2c9d520bcabccc7c1850
diodon <ekleins@gmail.com>
2020-02-18 13:28:21 +1100

    set dtypes from numpy

--- aodntools/timeseries_products/velocity_aggregated_timeseries.py
+++ aodntools/timeseries_products/velocity_aggregated_timeseries.py
@@ -208,8 +208,8 @@ def velocity_aggregated(files_to_agg, site_code, input_dir='', output_dir='./',
 
     obs_double_template = {'datatype': np.float64, 'zlib': True, 'dimensions': ('OBSERVATION'), "fill_value": 99999.0}
     obs_float_template = {'datatype': np.float32, 'zlib': True, 'dimensions': ('OBSERVATION'), "fill_value": 99999.0}
-    obs_byte_template = {'datatype': 'byte', 'zlib': True, 'dimensions': ('OBSERVATION'), 'fill_value': 99}
-    obs_int_template = {'datatype': 'int', 'zlib': True, 'dimensions': ('OBSERVATION')}
+    obs_byte_template = {'datatype': np.byte, 'zlib': True, 'dimensions': ('OBSERVATION'), 'fill_value': 99}
+    obs_int_template = {'datatype': np.uint32, 'zlib': True, 'dimensions': ('OBSERVATION')}
     inst_S256_template = {'datatype': 'str', 'dimensions': ('INSTRUMENT')}
     inst_float_template ={'datatype': np.float32, 'dimensions': ('INSTRUMENT')}
     inst_double_template ={'datatype': np.float64, 'dimensions': ('INSTRUMENT')}

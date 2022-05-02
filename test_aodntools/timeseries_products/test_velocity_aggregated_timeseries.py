e6ca5ddc88f13c1507a3812799b3a52a33083966
mhidas <marty.hidas@utas.edu.au>
2022-05-02 17:43:06 +1000

    small refactor of test_velocity_aggregated_timeseries.py

--- test_aodntools/timeseries_products/test_velocity_aggregated_timeseries.py
+++ test_aodntools/timeseries_products/test_velocity_aggregated_timeseries.py
@@ -22,8 +22,14 @@ EXPECTED_OUTPUT_FILE = os.path.join(
     TEST_ROOT, 'IMOS_ANMN-NRS_VZ_20180816_NRSROT_FV01_velocity-aggregated-timeseries_END-20191018_C-20200623.nc'
 )
 
+OBS_VARS = {'TIME', 'DEPTH', 'DEPTH_quality_control', 'UCUR', 'UCUR_quality_control',
+            'VCUR', 'VCUR_quality_control', 'WCUR', 'WCUR_quality_control', 'instrument_index', 'CELL_INDEX'}
+INST_VARS = {'LATITUDE', 'LONGITUDE', 'NOMINAL_DEPTH', 'SECONDS_TO_MIDDLE'}
+STR_VARS = {'source_file', 'instrument_id'}
+
+
 class TestVelocityAggregatedTimeseries(BaseTestCase):
-    def test_main_aggregator(self):
+    def test_velocity_aggregated(self):
         output_file, bad_files = velocity_aggregated(INPUT_FILES, 'NRSROT', input_dir=TEST_ROOT, output_dir='/tmp')
 
         self.assertEqual(1, len(bad_files))
@@ -34,19 +40,13 @@ class TestVelocityAggregatedTimeseries(BaseTestCase):
 
         # check dimensions and variables
         self.assertSetEqual(set(dataset.dimensions), {'OBSERVATION', 'INSTRUMENT', 'strlen'})
+        self.assertSetEqual(set(dataset.variables.keys()), OBS_VARS | INST_VARS | STR_VARS)
 
-        obs_vars = {'TIME', 'DEPTH', 'DEPTH_quality_control', 'UCUR', 'UCUR_quality_control',
-                    'VCUR', 'VCUR_quality_control', 'WCUR', 'WCUR_quality_control', 'instrument_index', 'CELL_INDEX'}
-        inst_vars = {'LATITUDE', 'LONGITUDE', 'NOMINAL_DEPTH', 'SECONDS_TO_MIDDLE'}
-        string_vars = {'source_file', 'instrument_id'}
-
-        self.assertSetEqual(set(dataset.variables.keys()), obs_vars | inst_vars | string_vars)
-
-        for var in obs_vars:
+        for var in OBS_VARS:
             self.assertEqual(dataset.variables[var].dimensions, ('OBSERVATION',))
-        for var in inst_vars:
+        for var in INST_VARS:
             self.assertEqual(dataset.variables[var].dimensions, ('INSTRUMENT',))
-        for var in string_vars:
+        for var in STR_VARS:
             self.assertEqual(dataset.variables[var].dimensions, ('INSTRUMENT', 'strlen'))
 
         for f in chartostring(dataset['source_file'][:]):

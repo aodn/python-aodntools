4167252d1ec97522ab135cde3b4379e431336acd
Stefan Hattrell <29941279+digorgonzola@users.noreply.github.com>
2022-09-06 14:21:49 +1000

    update bumpversion script and remove config file

--- bumpversion.sh
+++ bumpversion.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 
 main() {
   git fetch --prune origin "+refs/tags/*:refs/tags/*"
-  OLD_VERSION=$(git tag -l '*.*.*' --sort=-version:refname | head -n 1)
-  NEW_VERSION=$(bump2version --current-version $OLD_VERSION --list --tag --commit  --allow-dirty patch | grep -oP '^new_version=\K.*$')
-  git push origin tag $NEW_VERSION
+  NEW_VERSION=$(bump2version --current-version $(git describe) --list --no-commit --tag \
+  --tag-message 'Bump version to {new_version}' patch | grep -oP '^new_version=\K.*$')
+  git push origin tag v$NEW_VERSION
 
   exit 0
 }

80340a590d59527182fc07d6b135b089169cba27
Michael Hemming <mphemming@live.co.uk>
2025-04-07 14:29:03 +1000

    Updated because  the Validator no longer expects a parameter named `types`

--- aodntools/ncwriter/schema.py
+++ aodntools/ncwriter/schema.py
@@ -1,21 +1,20 @@
 """This module holds schema definitions for validating the various :py:class:`dicts` that make up parts of a
 template, and also the helper functions necessary to validate an object against their respective schema.
 """
-
 import json
-
 import numpy as np
 from jsonschema import validators, Draft4Validator, FormatChecker, ValidationError
 from pkg_resources import resource_filename
 
+def is_array(checker, instance):
+    return isinstance(instance, (list, np.ndarray))
 
-# Create a new validator class (based on Draft4Validator) to allow templates to use
-# * Python types or numpy dtypes to specify variable data types; and
-# * numpy arrays to specify variable data.
-TemplateValidator = validators.create(meta_schema=Draft4Validator.META_SCHEMA,
-                                      validators=Draft4Validator.VALIDATORS)
+# Extend the default type checker by redefining "array"
+custom_type_checker = Draft4Validator.TYPE_CHECKER.redefine("array", is_array)
+
+# Create a custom validator that uses the new type checker.
+CustomValidator = validators.extend(Draft4Validator, type_checker=custom_type_checker)
 format_checker = FormatChecker()
-
 
 @format_checker.checks('datatype')
 def is_python_datatype(value):
@@ -24,32 +23,25 @@ def is_python_datatype(value):
         return True
     if isinstance(value, type):
         return issubclass(value, np.number)
-
     return False
-
-
-TYPES = {'array': (list, np.ndarray)}
 
 TEMPLATE_SCHEMA_JSON = resource_filename(__name__, 'template_schema.json')
 with open(TEMPLATE_SCHEMA_JSON) as f:
     TEMPLATE_SCHEMA = json.load(f)
-TemplateValidator.check_schema(TEMPLATE_SCHEMA)
+CustomValidator.check_schema(TEMPLATE_SCHEMA)
 
-template_validator = TemplateValidator(TEMPLATE_SCHEMA, types=TYPES, format_checker=format_checker)
-
+# Use the custom validator
+template_validator = CustomValidator(TEMPLATE_SCHEMA, format_checker=format_checker)
 
 def validate_template(t):
     template_validator.validate(t)
 
-
 def validate_dimensions(d):
     validate_template({'_dimensions': d})
 
-
 def validate_variables(v):
     validate_template({'_variables': v})
-
-
+    
 def validate_global_attributes(a):
     if hasattr(a, 'keys'):
         special = [k for k in a.keys() if k.startswith('_')]

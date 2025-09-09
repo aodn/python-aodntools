36938d0846799e80f10b2dcb5d73040b328b4cb5
Michael Hemming <mphemming@live.co.uk>
2025-09-09 13:42:33 +1000

    revert change

--- test_aodntools/timeseries_products/test_hourly_timeseries.py
+++ test_aodntools/timeseries_products/test_hourly_timeseries.py
@@ -94,7 +94,6 @@ class TestHourlyTimeseries(BaseTestCase):
 
         self.compare_variables(dataset)
 
-
     def test_hourly_aggregator_with_nonqc(self):
         output_file, bad_files = hourly_aggregator(files_to_aggregate=INPUT_FILES,
                                                    site_code='NRSROT',
